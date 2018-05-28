8147b81dcb778b35674c716bf39cc09253fabaea
h.hyunwoo@gmail.com <Angles12>
2018-05-28 21:44:43 +0900

    prettier modified

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -1,97 +1,9 @@
 <template>
-  <div class="hello">
-    <h1></h1>
-    <h4></h4>
-    <div v-on:click="increment">[ + ]</div>
-    <div v-on:click="decrement">[ + ]</div>
-    <h2>Essential Links</h2>
-    <ul>
-      <li>
-        <a href="https://vuejs.org" target="_blank">
-          Core Docs
-        </a>
-      </li>
-      <li>
-        <a href="https://forum.vuejs.org" target="_blank">
-          Forum
-        </a>
-      </li>
-      <li>
-        <a href="https://chat.vuejs.org" target="_blank">
-          Community Chat
-        </a>
-      </li>
-      <li>
-        <a href="https://twitter.com/vuejs" target="_blank">
-          Twitter
-        </a>
-      </li>
-      <br>
-      <li>
-        <a href="https://github.com/hyunwoo/vue-template" target="_blank">
-          Github for This Template
-        </a>
-      </li>
-    </ul>
-    <h2>Ecosystem</h2>
-    <ul>
-      <li>
-        <a href="http://router.vuejs.org/" target="_blank">
-          vue-router
-        </a>
-      </li>
-      <li>
-        <a href="http://vuex.vuejs.org/" target="_blank">
-          vuex
-        </a>
-      </li>
-      <li>
-        <a href="http://vue-loader.vuejs.org/" target="_blank">
-          vue-loader
-        </a>
-      </li>
-      <li>
-        <a href="https://github.com/vuejs/awesome-vue" target="_blank">
-          awesome-vue
-        </a>
-      </li>
-    </ul>
-  </div>
+  <div class="hello"></div>
 </template>
 
 <script>
-export default {
-  name: 'HelloWorld',
-  data() {
-    return {
-      msg: 'Welcome to Your Vue.js App',
-      posX: 0,
-      posY: 0,
-    };
-  },
-  computed: {
-    count() {
-      return this.$store.state.count;
-    },
-    getPosition() {
-      return {
-        left: `${this.posX}px`,
-        top: `${this.posY}px`,
-      };
-    },
-  },
-  methods: {
-    increment() {
-      this.$store.commit('increment', { inc: 1 });
-    },
-    decrement() {
-      this.$store.commit('increment', { inc: -1 });
-    },
-    async mousemoved() {
-      console.log(this);
-    },
-  },
-};
+export default { name: 'HelloWorld' };
 </script>
 
 <!-- Add "scoped" attribute to limit CSS to this component only -->
